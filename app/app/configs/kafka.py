810d7464a34a3941cdcd785f52b33bbcda8b3957
xiaowei.song <songxiaowei@chinagoods.com>
2021-04-10 09:24:10 +0800

    topic名称修改

--- app/app/configs/kafka.py
+++ app/app/configs/kafka.py
@@ -2,4 +2,4 @@
 
 bootstrap_servers=['172.18.5.15:9092', '172.18.5.16:9092', '172.18.5.17:9092'] #Kafka服务器的地址
 client_group_id='events-tracking' #Kafka群组地址，同一群组共享一个Offset，不会重复，也不会漏。
-kafka_topic='events_tracking' #Kafka的Topic
+kafka_topic='events-tracking' #Kafka的Topic
