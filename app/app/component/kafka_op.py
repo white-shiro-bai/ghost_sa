cbfe7cfd3f75b4f977f0b71ab69307034e3ba6b5
stevan <734506700qq@gmail.com>
2021-08-27 23:11:32 +0800

    #Updates#准备部署，增加相关日志

--- app/app/component/kafka_op.py
+++ app/app/component/kafka_op.py
@@ -49,13 +49,13 @@ def insert_message_to_kafka(key, msg):
     else:
         key = None
     kafka_topic = current_app.config['KAFKA_TOPIC']
-    current_app.logger.info(f'即将往topic={kafka_topic}发送消息...')
+    current_app.logger.debug(f'即将往topic={kafka_topic}发送消息...')
     # 回补kafka_producer，避免断开连接
     if not hasattr(current_app, 'kafka_producer') or not current_app.kafka_producer:
         current_app.kafka_producer = CreateKafkaProducer().create_producer(current_app)
     future = current_app.kafka_producer.send(topic=kafka_topic, key=key, value=json.dumps(msg).encode())
     result = future.get(timeout=10)
-    current_app.logger.info(f'往topic={kafka_topic}发送消息完成, 结果为{result}')
+    current_app.logger.debug(f'往topic={kafka_topic}发送消息完成, 结果为{result}')
 
 
 # latest,earliest,none 首次拉取kafka订阅的模式
